88533ab0abb46c6f55a7fa2084353962f44b0c9b
Rishab <rishabdakhale17@gmail.com>
2025-12-01 00:20:22 +0530

    changes done

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,21 +2,12 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* PROTECTED ROUTES */}
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
