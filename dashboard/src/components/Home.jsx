45e288de0e0c0880ace22c15c7a68fc77533db0a
Rishab <rishabdakhale17@gmail.com>
2025-12-04 01:09:49 +0530

    done

--- dashboard/src/components/Home.jsx
+++ dashboard/src/components/Home.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import { useEffect } from "react";
 
 import Dashboard from "./Dashboard ";
 import TopBar from "./TopBar";
 import ProtectedRoute from "../ProtectedRoute";
 
 const Home = () => {
+  useEffect(() => {
+    const token = new URLSearchParams(window.location.search).get("token");
+    if (token) {
+      localStorage.setItem("token", token);
+      window.history.replaceState({}, "", "/"); // remove token from url
+    }
+  }, []);
   return (
     <>
+      <TopBar />
       <ProtectedRoute>
-        <TopBar />
         <Dashboard />
       </ProtectedRoute>
     </>
